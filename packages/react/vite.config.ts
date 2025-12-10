b98d784afcae3a28d3df5c603df9cdaac88e6542
EralChen <eralchen@foxmail.com>
2025-12-10 15:19:01 +0800

    feat: add Rollup replace plugin to handle worker module paths

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,6 +1,7 @@
 import { existsSync, readdirSync, statSync } from 'node:fs'
 import { join, resolve } from 'node:path'
 import { distDir } from '@lib-env/path'
+import replace from '@rollup/plugin-replace'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
@@ -19,6 +20,7 @@ for (const dir of dirs) {
 }
 
 export default defineConfig({
+
   build: {
     lib: {
       entry: entries,
@@ -34,11 +36,33 @@ export default defineConfig({
         '@vunk/markdown',
       ],
     },
+
   },
+
   plugins: [
     vue(),
     dts({
       tsconfigPath: resolve(__dirname, 'tsconfig.json'),
+    }),
+
+    /* [TODO] 需要更好的解决方案
+      vite lib 打包 ts， 产物生成了
+      ```ts
+      spawnWorker() {
+        this.proxy = MHe(
+          new Worker(new URL( "/worker-xxx.js", import.meta.url ), { type: "module" }) ),
+          this.running && (this.running = !1, this.execute()
+        );
+      }
+      ```
+      导致别人使用的时候报错了
+      ✗ Build failed in 1m 21s error during build: [vite:worker-import-meta-url] Could not resolve entry module "public/worker-xxx.js"
+    */
+    replace({
+      values: {
+        '/worker-': './worker-',
+      },
+      delimiters: ['', ''],
     }),
   ],
 })
